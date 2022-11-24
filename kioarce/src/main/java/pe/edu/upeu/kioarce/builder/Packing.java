ac399f7d95f404117276fc196a883bd19d96568e
Samuel-910 <dasarupazfesandra@gmail.com>
2022-11-23 21:34:14 -0500

    nel

--- kioarce/src/main/java/pe/edu/upeu/kioarce/builder/Packing.java
+++
@@ -1,9 +0,0 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
-package pe.edu.upeu.kioarce.builder;
-
-public interface Packing {
-   public String pack();
-}
